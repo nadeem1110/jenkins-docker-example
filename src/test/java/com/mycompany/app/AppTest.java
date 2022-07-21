f6af93d29cf9c27ad0316bad1852efa43f007c9e
nadeem1110 <103575023+nadeem1110@users.noreply.github.com>
2022-07-21 12:55:34 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,13 @@
-package com.mycompany.app;
+package com.javatechie;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import org.junit.Test;
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
